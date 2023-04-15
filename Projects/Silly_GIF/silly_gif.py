aec5f85b141d1b0ea05ec3d7be8e9428fee53b77
JBSpirit <JBSpiritous@gmail.com>
2023-04-15 18:54:08 -0500

    code update 4-15-2023

--- Projects/Silly_GIF/silly_gif.py
+++ Projects/Silly_GIF/silly_gif.py
@@ -1,52 +1,68 @@
+"""
+This module is used to create a GIF file from a text message.
+"""
 import os
-from moviepy.editor import *
+from moviepy.editor import (TextClip, CompositeVideoClip, ColorClip,
+                            ImageClip, ImageSequenceClip)
 
 # Set up font and message
-font_path = "Caveat-Bold.ttf"
-font_size = 50
-font_color = "white"
-message = "You are amazingly silly"
+FONT_PATH = "Caveat-Bold.ttf"
+FONT_SIZE = 50
+FONT_COLOR = "white"
+MESSAGE = "You are amazingly silly!"
 
 # Set up background color and size
-bg_color = "black"
-width, height = 500, 500
+BG_COLOR = "black"
+WIDTH, HEIGHT = 500, 500
 
 # Set up duration and number of repetitions
-duration = 30
-n_repetitions = duration // 3
+DURATION = 30
+N_REPETITIONS = DURATION // 3
 
 # Set up path to ImageMagick convert binary
-imagemagick_path = r"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\convert.exe"
-ffmpeg_binary = r"C:\ffmpeg\bin\ffmpeg.exe"
+IMAGEMAGICK_PATH = r"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\convert.exe"
 
 # Create the text clip
-text_clip = TextClip(message, fontsize=font_size, font=font_path, color=font_color)
+TEXT_CLIP = TextClip(MESSAGE, fontsize=FONT_SIZE, font=FONT_PATH, color=FONT_COLOR)
 
 # Set the duration of the text clip
-text_clip = text_clip.set_duration(duration)
+TEXT_CLIP = TEXT_CLIP.set_duration(DURATION)
 
 # Animate the text clip
-animation = (text_clip.set_position("center")
-                       .fadein(2)
-                       .fadeout(2))
+ANIMATION_CLIP = TEXT_CLIP.set_position("center").fadein(2).fadeout(2)
 
-for i in range(n_repetitions):
-    animation = animation.to_ImageClip(1).set_duration(1)
-    animation.write_videofile(filename="temp.mp4", fps=15, codec="libx264", ffmpeg_params=["-pix_fmt", "yuv420p", "-preset", "medium", "-b:v", "1000k", "-b:a", "192k"])
-    animation = VideoFileClip("temp.mp4", ffmpeg_binary=ffmpeg_binary)
+# Create the directory for the image files
+IMAGE_DIR = os.path.join(os.getcwd(), "images")
+if not os.path.exists(IMAGE_DIR):
+    os.makedirs(IMAGE_DIR)
+
+# Generate the image files for the animation
+for i, t in enumerate(range(DURATION * 15)):
+    img_path = os.path.join(IMAGE_DIR, f"frame_{i:04d}.png")
+    ANIMATION_CLIP.to_ImageClip(t / 15.0).save_frame(img_path)
+
+# Create the image sequence clip from the generated image files
+ANIMATION_CLIP = ImageSequenceClip(os.path.join(IMAGE_DIR, "frame_%04d.png"), fps=15)
+
+# Repeat the animation and add each repetition to the video file
+ANIMATION_CLIP = ANIMATION_CLIP.set_duration(ANIMATION_CLIP.duration * N_REPETITIONS)
 
 # Create the background clip
-bg_clip = ColorClip(size=(width, height), color=bg_color)
+bg_clip = ColorClip(size=(WIDTH, HEIGHT), color=BG_COLOR)
 
 # Combine the animation and background clips
-final_clip = CompositeVideoClip([bg_clip, animation])
+FINAL_CLIP = CompositeVideoClip([bg_clip, ANIMATION_CLIP])
 
 # Set the path to where the GIF will be saved
-gif_path = os.path.join(r"C:\Code-Repository\VS Code Projects\Pratice Code Project\Test Projects\Silly_GIF", "you-are-amazingly-silly.gif")
+GIF_PATH = os.path.join(os.getcwd(), "you-are-amazingly-silly.gif")
 
 # Write the GIF file
-final_clip.write_gif(gif_path, fps=15, program=imagemagick_path, opt="optimizeplus")
+FINAL_CLIP.write_gif(GIF_PATH, fps=15, program=IMAGEMAGICK_PATH, opt="optimizeplus")
 
 # Print the path of the saved GIF
-print("GIF saved at:", gif_path)
+print("GIF saved at:", GIF_PATH)
 
+# Delete the image files
+for f in os.listdir(IMAGE_DIR):
+    os.remove(os.path.join(IMAGE_DIR, f))
+os.rmdir(IMAGE_DIR)
